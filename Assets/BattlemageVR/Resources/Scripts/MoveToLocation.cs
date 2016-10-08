9f4e1cc1f798de14dadb4a0f5e0c1ade19618fd4
Kevin Gray <kevingraybusiness@gmail.com>
2016-10-08 17:33:30 -0500

    Archers, warriors, spawn zones

--- Assets/BattlemageVR/Resources/Scripts/MoveToLocation.cs
+++ Assets/BattlemageVR/Resources/Scripts/MoveToLocation.cs
@@ -1,20 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoveToLocation : MonoBehaviour {
+public class MoveToLocation : MonoBehaviour
+{
     public Vector3 TargetPosition = new Vector2();
-    public float Speed = 1;
+    public float MaxSpeed = 1;
+    public float Force = .01f;
 
-	// Update is called once per frame
-	void Update () {
-        var moveAmount = Speed * Time.deltaTime;
-        var direction = TargetPosition - transform.position;
+    // Update is called once per frame
+    void Update () {
+        //var moveAmount = Speed * Time.deltaTime;
+        //var direction = TargetPosition - transform.position;
+        //
+        //if(moveAmount > direction.magnitude)
+        //{
+        //    moveAmount = direction.magnitude;
+        //}
+        //
+        //transform.position += moveAmount * direction.normalized;
 
-        if(moveAmount > direction.magnitude)
+        var force = Force * Time.deltaTime;
+        var direction = (TargetPosition - transform.position).normalized;
+        var body = gameObject.GetComponent<Rigidbody>();
+        body.AddForce(direction * force);
+
+        if(body.velocity.magnitude > MaxSpeed)
         {
-            moveAmount = direction.magnitude;
+            body.velocity = body.velocity.normalized * MaxSpeed;
         }
-
-        transform.position += moveAmount * direction.normalized;
-	}
+    }
 }
